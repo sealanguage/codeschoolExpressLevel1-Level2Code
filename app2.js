4914a1d50df2309a3ad457f3ccb089126dc0a0b8
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-03-06 23:30:07 +0000

    expressLevel4 initial commit not woking well yet

--- app2.js
+++ app2.js
@@ -1,21 +1,47 @@
 //  Express Level 2 homework
-/* global require */
+/* global require request */
 
 var express = require('express');
 // will check the node+modules folder to find node.js. it assumes this is js file because this is javascript
 var app = express();
-//var client = require('client');
-//app.use(client);
+var cities = {
+    'Altoona': 'PA', 
+    'Ligonier': 'PA', 
+    'Seward': 'PA', 
+    'Cumberland': 'MD', 
+    'Windber': 'PA', 
+    'Bedford': 'PA'
+    };
 
-
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/cities/:name', function(reqeust, response) {
+    var name = cities[request.params.name];
+    var cities = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    if (!name) {
+        response.status(404).json('State not found for ' + request.params.name);
+    } else {
+        response.json(name);
+    }
+});
+
 app.get('/cities', function(request, response) {
-    var cities = ['Altoona', 'Ligonier', 'Seward', 'Cumberland'];
-    response.json(cities);
+    if(request.query.limit >= 0) {
+        response.json(cities.slice(0, request.query.limit));
+    } else {
+        response.json(Object.keys(cities));
+    }
+});
+
+app.post('/cities', parseUrlencoded, function(request,response) {
+    var newCity = request.body;
+    cities[newCity.name] = newCity.state
+    response.status(201).json(newCity.name);
 });
 
 app.use(express.static('public'));
@@ -25,14 +51,29 @@ app.use(express.static('public'));
 // });
 
 app.listen(process.env.PORT, function() {
-console.log('Listening on port new client file');
+console.log('Listening from app2.js');
 });
-//   ckeck  Create a /cities route in your app.js file with at least 4 cities.
-//   ckeck  Create an index route that refers to the following file requirement.
-//   ckeck  Create index.html in a folder called public.
-// Index.html
-//   ckeck  Include an H1 header
-//   ckeck  Include a form with an empty selection element
-//   ckeck  Link up a js file.
-//   ckeck  Create JS file for the index.html file that will make an ajax request 
-//   to /cities and display each city inside the selection element.
+
+
+
+
+        // Add the ability to add a new city to your list.
+        //              City name
+        //              Containing city
+        //              Should confirm the city and state has values
+        
+        //      State should have at least two characters.
+        //      City should have at least four characters
+        //      Page should update to hide the city
+        //      Update page to make the cities links which return the state name
+        //      Add ability to remove a city
+        //      City should be confirmed prior to removing it
+        
+        //          Response should contain the correct status code
+        //          Page should update with new info
+        
+        // Additional challenge
+        // Add additional information to the site:
+        //              Add the additional data points longitude and latitude
+        //              Clickable link should display google maps centered to the longitude and latitude in new tab
+        //              Form should confirm that lat lon are numbers.
--- public/client.js
+++ public/client.js
@@ -6,9 +6,25 @@ $ (function () {
     function appendToOptions(cities) {
         var options = [];
         for (var i in cities) {
-            options.push($('<option>', {text: cities[i]}));
+            //options.push($('<option>', {text: cities[i]}));
+            var content = '<a href="/cities/' + cities + '">' + cities + '</a>';
+            options.push($('<option>', {html:content}));
         }
     $('#cities-list').append(options);
-    }
+    };
     
+$('form').on('submit', function(event) {
+    event.preventDefault();
+    var form = $(this);
+    var cityData = form.serialize();
+ 
+ // cityName  
+    $.ajax ({
+        type: 'POST', url:'/cities', data:cityData}).done(function (cityName) {
+            appendToOptions([cityName]);
+        });
+        form.trigger('reset');
+    });
 });
+    
+//});
